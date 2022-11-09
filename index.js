d5c768cc615fdef48688f4c97b86d0f7835b5620
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-09 22:03:33 +0600

    set node mongo server connect and get data from db

--- index.js
+++ index.js
@@ -12,14 +12,24 @@ app.use(express.json());
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.etkkvpu.mongodb.net/?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
-client.connect(err => {
-    const collection = client.db("test").collection("devices");
-    // perform actions on the collection object
-    client.close();
-});
 
+async function run() {
+    try {
+        const serviceCollection = client.db('unicornEmedicine').collection('services');
+
+        app.get('/services', async (req, res) => {
+            const query = {};
+            const cursor = serviceCollection.find(query);
+            const services = await cursor.toArray();
+            res.send(services);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(error => console.error());
 
 app.get('/', (req, res) => {
     res.send('Unicorn is running away...')
