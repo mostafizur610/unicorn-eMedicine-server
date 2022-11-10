e008b6c058ea5a4d6b6919fc0f61ac88170d48a0
mostafizur610 <mostafizurrahman273@gmail.com>
2022-11-10 23:23:11 +0600

    jwt token implemented

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken')
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 require('dotenv').config();
 
@@ -14,10 +15,32 @@ app.use(express.json());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.etkkvpu.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
+function verifyJWT(req, res, next) {
+    // console.log(req.headers.authorization);
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, function (err, decoded) {
+        if (err) {
+            return res.status(403).send({ message: 'Forbidden access' })
+        }
+        req.decoded = decoded;
+        next();
+    })
+}
+
 async function run() {
     try {
         const serviceCollection = client.db('unicornEmedicine').collection('services');
         const reviewCollection = client.db('unicornEmedicine').collection('reviews');
+
+        app.post('/jwt', async (req, res) => {
+            const user = req.body;
+            const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1d' });
+            res.send({ token });
+        })
 
         app.get('/services', async (req, res) => {
             const limit = parseInt(req.query.limit);
@@ -35,8 +58,11 @@ async function run() {
 
         app.get('/serviceDetails/:id', async (req, res) => {
             const id = new ObjectId(req.params.id);
-            const query = { _id: id }
-            const serviceDetails = await serviceCollection.findOne(query);
+            const serviceQuery = { _id: id }
+            const reviewQuery = { serviceId: id }
+            const serviceDetails = await serviceCollection.findOne(serviceQuery);
+            const reviews = await reviewCollection.find(reviewQuery).toArray();
+            serviceDetails.reviews = reviews;
             // console.log(serviceDetails);
             res.send(serviceDetails);
         });
@@ -68,7 +94,7 @@ async function run() {
             res.send(data);
         });
 
-        app.get('/review', async (req, res) => {
+        app.get('/review', verifyJWT, async (req, res) => {
             // console.log(req.query);
             const email = req.query.email;
             const query = { email: email }
@@ -80,19 +106,25 @@ async function run() {
             res.send(reviewData);
         });
 
+        app.get('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: new ObjectId(id) }
+            const reviewData = await reviewCollection.findOne(query);
+            res.send(reviewData);
+        });
+
         // update
-        // app.patch('/review/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     const status = req.body.status;
-        //     const query = { _id: ObjectId(id) }
-        //     const updatedDoc = {
-        //         $set: {
-        //             status: status
-        //         }
-        //     }
-        //     const result = await orderCollection.updateOne(query, updatedDoc);
-        //     res.send(data);
-        // })
+        app.put('/review/:id', async (req, res) => {
+            const id = req.params.id;
+            const rating = req.body.rating;
+            const review = req.body.review;
+            const query = { _id: ObjectId(id) }
+            const updatedDoc = {
+                $set: { rating, review }
+            }
+            const result = await reviewCollection.updateOne(query, updatedDoc);
+            res.send(result);
+        })
 
         // delete row
         app.delete('/review/:id', async (req, res) => {
